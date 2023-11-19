9a00785f78d25814091c5fafb638f877dce4a357
Sarah de Paula <126208084+SarahTech4@users.noreply.github.com>
2023-11-19 16:41:46 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,71 @@
+# Criando uma calculadora em Python com interface!! 
+from tkinter import * # -> Fazendo a importação do tkinter para criar interfaces :D (GOXTEI)
+from tkinter import ttk
+
+# Cores da nossa calculadora linda e maravilhosa
+cor1 = '#1e1f1e' # preta
+cor2 = '#feffff' # white/branco
+cor3 = '#38576b' # azul carregado
+cor4 = '#ECEFF1' # cizenta
+cor5 = '#FFAB40' # orange/laranja
+
+janela = Tk()
+janela.eval('tk::PlaceWindow %s center' % janela.winfo_pathname(janela.winfo_id()))
+janela.title('Calculadora')
+janela.geometry('235x310')
+janela.config(bg= cor1)
+
+frame_tela = Frame(janela, width=235, height=50, bg= cor3)
+frame_tela.grid(row=0, column=0)
+
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
+
+# Criando botões
+b_1 = Button(frame_corpo, text='C', width=11, height=2, bg= cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_1.place(x=0, y=0)
+b_2 = Button(frame_corpo, text='%', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_2.place(x=118, y=0)
+b_3 = Button(frame_corpo, text='/', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_3.place(x=177, y=0)
+
+b_4 = Button(frame_corpo, text='7', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_4.place(x=0, y=52)
+b_5 = Button(frame_corpo, text='8', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_5.place(x=59, y=52)
+b_6 = Button(frame_corpo, text='9', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_6.place(x=118, y=52)
+b_7 = Button(frame_corpo, text='*', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_7.place(x=177, y=52)
+
+b_8 = Button(frame_corpo, text='4', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_8.place(x=0, y=104)
+b_9 = Button(frame_corpo, text='5', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_9.place(x=59, y=104)
+b_10 = Button(frame_corpo, text='6', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_10.place(x=118, y=104)
+b_11 = Button(frame_corpo, text='-', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_11.place(x=177, y=104)
+
+b_12 = Button(frame_corpo, text='1', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_12.place(x=0, y=156)
+b_13 = Button(frame_corpo, text='2', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_13.place(x=59, y=156)
+b_14 = Button(frame_corpo, text='3', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_14.place(x=118, y=156)
+b_15 = Button(frame_corpo, text='+', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_15.place(x=177, y=156)
+
+b_16 = Button(frame_corpo, text='0', width=11, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_16.place(x=0, y=208)
+b_17 = Button(frame_corpo, text='.', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_17.place(x=118, y=208)
+b_18 = Button(frame_corpo, text='=', width=5, height=2, bg=cor4, font=('Ivy 13 bold'),relief=GROOVE, overrelief=GROOVE)
+b_18.place(x=177, y=208)
+
+
+
+
+janela.mainloop()
+
